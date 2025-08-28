f94a8ea7a1ed5f98fc7cff8e5c18ee15030b8036
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-08-28 17:02:37 -0300

    fix: fixing problem sactum

--- config/sanctum.php
+++ config/sanctum.php
@@ -16,10 +16,9 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s%s',
+        '%s,%s',
         'localhost,localhost:3000,localhost:5173,127.0.0.1,127.0.0.1:8000,127.0.0.1:5173,::1,atendebem.netlify.app',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
+        Sanctum::currentApplicationUrlWithPort()
     ))),
 
     /*
