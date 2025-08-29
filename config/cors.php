ceb802f039cb1c77ef8efd7631358cb22b7dc63c
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-08-28 23:09:48 -0300

    fix: fixing cors problem

--- config/cors.php
+++ config/cors.php
@@ -21,29 +21,19 @@ return [
 
     'allowed_origins' => [
         'https://atendebem.netlify.app',
-        'https://www.atendebem.netlify.app',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost:8080',
-        'http://127.0.0.1:8080',
     ],
 
+
     'allowed_origins_patterns' => [
-        'https://.*\.netlify\.app',
+        '/^https:\/\/.*\.netlify\.app$/',
     ],
+
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [
-        'Authorization',
-        'Content-Type',
-        'X-Requested-With',
-        'Access-Control-Allow-Origin',
-        'Access-Control-Allow-Headers',
-        'Access-Control-Allow-Methods',
-    ],
+    'exposed_headers' => [],
 
     'max_age' => 86400,
 
