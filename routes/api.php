499e0322b75cf3f529a47e0e9450b401d9936ccc
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-10-21 23:09:22 -0300

    fix: namespace reset password

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ Route::middleware('guest')->group(function () {
     // email reset password
     Route::post('/reset-password-email', [AtendeBemMail::class, 'sendTokenEmail']);
 
-    Route::post('/reset-password-user', [ResetPassword::class, 'reset_password']);
+    Route::post('/reset-password-user', [App\Http\Controllers\Api\Mail\ResetPassword::class, 'reset_password']);
 
     Route::get('/health', function () {
         try {
